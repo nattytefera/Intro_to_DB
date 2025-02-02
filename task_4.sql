79b269e70c48bdf43abb51e341379594f4b1f901
nattytefera <nhattysafari@gmail.com>
2025-02-02 23:57:30 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,22 @@
--- Get full details of the BOOKS table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'BOOKS';
+-- task_4.sql
+-- This script retrieves the full description of the 'Books' table
+-- from the 'alx_book_store' database
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    CHARACTER_MAXIMUM_LENGTH, 
+    NUMERIC_PRECISION, 
+    NUMERIC_SCALE, 
+    DATETIME_PRECISION, 
+    COLUMN_KEY, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
+
+mysql -u your_username -p alx_book_store < task_4.sql
