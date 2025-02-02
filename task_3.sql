fa930d5221fdb921ea472c720891c311ac206c71
nattytefera <nhattysafari@gmail.com>
2025-02-02 23:57:08 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,8 @@
--- List all tables in the database
+i-- task_3.sql
+-- This script lists all tables in the given MySQL database
+
+USE alx_book_store;
+
 SHOW TABLES;
+
+mysql -u your_username -p your_database_name < task_3.sql
