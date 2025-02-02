63ed0087646eac29ade6a8c0d10b21a9934ff36f
nattytefera <nhattysafari@gmail.com>
2025-02-02 23:58:05 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,12 @@
--- Insert multiple rows into the CUSTOMERS table
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- task_6.sql
+
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address) 
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+
+mysql -u root -p alx_book_store < task_6.sql
+
