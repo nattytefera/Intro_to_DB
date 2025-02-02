9ca216e86afa249e40064cd475e68f7394308457
nattytefera <nhattysafari@gmail.com>
2025-02-02 23:57:46 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,8 @@
--- Insert a single row into the CUSTOMERS table
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+-- task_5.sql
+-- This script inserts a single row into the 'customer' table
+-- in the 'alx_book_store' database
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+mysql -u your_username -p alx_book_store < task_5.sql
